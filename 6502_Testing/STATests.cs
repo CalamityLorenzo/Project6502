df804c9919e4c960be74a78746498766fb6cdbf8
CalamityLorenzo <CalamityLorenzo@users.noreply.github.com>
2023-04-08 21:15:05 +0100

    asddsadsa

--- 6502_Testing/STATests.cs
+++ 6502_Testing/STATests.cs
@@ -23,8 +23,8 @@ namespace _6502_Testing
                 0xA9, // LDA 200
                 0xC8,
 
-                0x85, // STA
-                0xA2, 
+                0x85, // STA 
+                0xA2,
             };
 
             processor.Process(program);
@@ -41,19 +41,19 @@ namespace _6502_Testing
         public void STA_ZeroPage_X()
         {
             var memory = new byte[ushort.MaxValue];
-            
-            memory[162] = 100;
+
+            //memory[162] = 100;
             var processor = createProcessor(memory);
             var program = new byte[]
             {
                 0xA9, // LDA 64
                 0x40,
-                0xAA, // Put in X TAX
+                0xAA, // Put 64 in X TAX
 
                 0xA9, // LDA 200
                 0xC8,
 
-                0x95, // STA
+                0x95, // STA -> (A2+64) = e2 = 200;
                 0xA2,
             };
 
@@ -62,9 +62,37 @@ namespace _6502_Testing
             var registers = processor.Registers();
 
             Trace.WriteLine($"A {registers["A"]}");
-            Trace.WriteLine($"mem[100] =  {memory[100 +0x40] }");
-            Assert.IsTrue(memory[100 + 0x40] == 0xC8);
+            Trace.WriteLine($"mem[{(0xA2 + 0x40)}] =  {memory[0xA2 + 0x40]}");
+            Assert.IsTrue(memory[0xA2 + 0x40] == 0xC8);
+        }
+
+        [TestMethod("STA : Absolute")]
+        public void STA_Absolute()
+        {
+
+            var memory = new byte[ushort.MaxValue];
+
+            var processor = createProcessor(memory);
+            var program = new byte[]
+            {
+                0xA9, // LDA 200
+                0xC8,
+
+                0x8D, // STA 
+                0xA2, // Remeber A full 16bit address = 0xA240 (Not 0xA2 + 0x40)
+                0x40
+            };
+
+            processor.Process(program);
+
+            var registers = processor.Registers();
+
+            Trace.WriteLine($"A {registers["A"]}");
+            var memAddress = (0xA2 << 8 | 0x40);
+            Trace.WriteLine($"mem[{memAddress}] =  {memory[memAddress]}");
+            Assert.IsTrue(memory[memAddress] == 200);
 
         }
+
     }
 }
